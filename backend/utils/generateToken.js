efa96bfdce369a524b290d252d6e77eb8233c250
RohitVerma2003 <rvermajio78300@gmail.com>
2025-12-03 17:48:15 +0530

    lax problem fixed

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCokkie = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "true",
+        sameSite: "lax",
         secure : process.env.NODE_ENV !== 'development'
     });
 
