dad640d60fe59a04e8c8a1b5536a148e15465785
RohitVerma2003 <rvermajio78300@gmail.com>
2025-12-03 23:30:12 +0530

    for deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors({
     credentials: true
 }))
 
-// const __dirname = path.resolve()
+const __dirname = path.resolve()
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -30,10 +30,10 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/channels", channelRoutes);
 
-// app.use(express.static(path.join(__dirname , "/frontend/dist")))
-// app.get("*" , (req , res)=>{
-//     res.sendFile(path.join(__dirname , "frontend" , "dist" , "index.html"))
-// })
+app.use(express.static(path.join(__dirname , "/frontend/dist")))
+app.get("*" , (req , res)=>{
+    res.sendFile(path.join(__dirname , "frontend" , "dist" , "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
